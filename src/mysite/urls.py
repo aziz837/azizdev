5df57c917c72aba735e745e70a8f67480813dc40
aziz837 <mubashirov2002@gmail.com>
2021-05-04 22:04:43 +0500

    aaaa

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.views.static import serve
-from django.urls import path, include, url
+from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import  url
 from django.conf.urls.static import static
 
 urlpatterns = [
